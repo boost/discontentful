e4d907e697ca54b3e116867edbf1cdf03b6dc278
Andy Anastasiadis-Gray <andy@boost.co.nz>
2022-10-27 12:02:45 +1300

    feat: new gem with bundler

---
+++ lib/contentful/transformation/toolkit/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Contentful
+  module Transformation
+    module Toolkit
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/contentful/transformation/toolkit_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Contentful::Transformation::Toolkit do
+  it "has a version number" do
+    expect(Contentful::Transformation::Toolkit::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
